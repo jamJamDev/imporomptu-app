a56c492a7543b991df5406034b9ab52ce2a77a20
unknown <keenangoldis@gmail.com>
2017-09-16 15:45:46 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from "./Button.js"
+import MapPage from "./MapPage.js"
 import './App.css';
 
 class App extends Component {
@@ -8,11 +10,17 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Retestact</h2>
+          <h2>This is our main page</h2>
+          <Button text="Click Me!" color="red"/>
+          <Button text="Press Me!" color="blue"/>
+          <Button text="Not Today!" color="green"/>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <MapPage />
+        </div>
       </div>
     );
   }
