6264d63d1148b1b5d12c0a5f9be2c8ebff4898f6
Jamie Thompson <jthompson@geo-jobe.com>
2017-09-17 10:22:31 -0500

    Fixed dummy data. Aligned map & list correctly

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ class Map extends Component {
 
     render() {
         return (
-            <div style={{width:900, height:900}}>
+            <div style={{width:1400, height:950, float:"left", marginRight: 8}}>
                 <GoogleMap
                     bootstrapURLKeys={{
                         key: "AIzaSyCbwEpFYJn7XRyB3RiWHVgKSgqO7TsdqXU",
