c61d79541a96708f10aabaaed701b17d2d72f55e
kristastarr <kstarr0092@gmail.com>
2017-09-17 13:01:23 -0500

    style create event form

--- src/components/CreateEventPage.js
+++ src/components/CreateEventPage.js
@@ -1,36 +1,75 @@
 import React from 'react';
-import Calendar from 'react-input-calendar'
+import 'mui-react/dist/mui-react.css';
+import {Button, Appbar, Container} from 'mui-react';
+
+
+class CreateEvent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      date: '',
+      location: '',
+      description: '',
+      contact: ''
+
+      };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+   
+    this.setState({title: event.target.title});
+    this.setState({date: event.target.tags}); 
+    this.setState({date: event.target.date}); 
+    this.setState({location: event.target.location});
+    this.setState({description: event.target.description});
+    this.setState({contact: event.target.contact});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.date);
+    event.preventDefault();
+  }
 
   render() {
     return (
-      <div>
+      <div style={{border: "1px solid black", margin: "auto", width: "375px", padding: "10px 30px 10px 50px"}}>
       <h1>Add an Event</h1>
       <form onSubmit={this.handleSubmit}>
         <label>
-          Event Title:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label> < br /> 
+          Title:
+          <input id="input" name="title" type="text" value={this.state.title} onChange={this.handleChange} />
+        </label> <br />
+        < label >
+         Tags:
+          <input id="input" name="tags" type="text" value={this.state.tags} onChange={this.handleChange} />
+        </label> <br />
           <label>
-          Date/Time:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>  < br /> 
+          Date:
+          <input name="date" type="text" value={this.state.date} onChange={this.handleChange} />
+        </label> <br />
         <label>
           Location:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>  < br /> 
+          <input  name="location" type="text" value={this.state.location} onChange={this.handleChange} />
+        </label> <br />
         <label>
           Description:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>  < br /> 
+          <input name="description" type="text" value={this.state.description} onChange={this.handleChange} />
+        </label> <br /> 
         <label>
           Contact Info:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>  < br /> 
-        <input type="submit" value="Submit" />
+          <input name="contact" type="text" value={this.state.contact} onChange={this.handleChange} />
+        </label> <br />
+        <input style={{backgroundColor:"#14ad24 ", outline:"none", color:"white"}} type="submit" value="Submit" />
       </form>
       </div>
     );
   }
 }
 
-export default NameForm
+
+export default CreateEvent
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import promise from 'redux-promise';
 import './index.css';
 
