c0edbe5d75f0fadc2d721efffa4893f3bb8b60f8
Jamie Thompson <jthompson@geo-jobe.com>
2017-09-17 10:00:18 -0500

    got data to show up! :D

--- src/components/EventListItem.js
+++ src/components/EventListItem.js
@@ -3,14 +3,12 @@ import React from 'react';
 const EventListItem = ({eventData}) => {
     console.log(eventData);
     return (
-        <li className="list-group-item">
-            <div className="video-list media">
-                <div className="media-body">
-                    <h1>{eventData.title}</h1>
-                    <div>Description: {eventData.description}</div>
-                </div>
+        <div className="video-list media list-group-item">
+            <div className="media-body">
+                <h1>{eventData.eventTitle}</h1>
+                <div>Description: {eventData.description}</div>
             </div>
-        </li>
+        </div>
     )
 };
 
--- src/containers/EventList.js
+++ src/containers/EventList.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EventListItem from '../components/EventListItem';
 import { fetchEvents } from "../actions/index";
+import _ from 'lodash';
 
-const EventList = (props) => {
-    const data = fetchEvents();
-    console.log(data);
-    const eventItems = data.payload.events.forEach((eventData) => {
-        return (
-            <EventListItem
-                key={eventData.eventID}
-                eventData={eventData} />
+class EventList extends Component {
+    componentDidMount() {
+        this.props.fetchEvents();
+    }
+
+    renderEvents(){
+        console.log(this.props.fetchEvents());
+        console.log(this.props);
+        console.log(this.state)
+        return _.map(this.props.fetchEvents().payload.events, eventData => {
+            console.log("EVENT DATA:", eventData);
+            return (
+                <EventListItem
+                    key={eventData.eventID}
+                    eventData={eventData} />
+            );
+        });
+        /*eventItems = data.payload.events.forEach((eventData) => {
+            return (
+                <EventListItem
+                    key={eventData.eventID}
+                    eventData={eventData} />
+            );
+        });*/
+    }
+
+    render() {return(
+            <ul className="col-md-4 list-group">
+                {this.renderEvents()}
+            </ul>
         );
-    });
-
-    return(
-        <ul className="col-md-4 list-group">
-            {eventItems}
-        </ul>
-    );
+    }
 }
 
-function mapStateToProps({ eventData }) {
-    return { eventData };
+function mapStateToProps(state) {
+    console.log(state);
+    return { eventData: state.eventData };
 }
 
-export default connect(mapStateToProps)(EventList);
+export default connect(mapStateToProps, { fetchEvents })(EventList);
